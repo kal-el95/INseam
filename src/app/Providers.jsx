0a14e6d80bd7c5c0efed5a2308fd01a47983ed76
Avdhoot Kasture <45avdhootkasture8@gmail.com>
2024-06-23 19:42:43 +0530

    Add Dark Moe Functionality

---
+++ src/app/Providers.jsx
@@ -0,0 +1,12 @@
+'use client';
+import {ThemeProvider} from 'next-themes'
+
+export default function Providers({children}) {
+  return (
+    <ThemeProvider defaultTheme='system' attribute='class'>
+      <div className='text-gray-900 dark:text-gray-300 dark:bg-gray-900 min-h-screen select-none transition-colors duration-500'>
+      {children}
+      </div>
+    </ThemeProvider>
+  )
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import MenuItem from './MenuItem';
 import {AiFillHome} from 'react-icons/ai';
 import {BsFillInfoCircleFill} from 'react-icons/bs';
+import DarkModeSwitch from './DarkModeSwitch';
 export default function Header() {
   return (
     <div className='flex justify-between items-center p-3 max-w-7xl mx-auto'>
@@ -9,9 +10,12 @@ export default function Header() {
         <MenuItem title='Home' address='/' Icon={AiFillHome} />
         <MenuItem title='About' address='/about' Icon={BsFillInfoCircleFill} />
       </div>
+      <div className='flex items-center gap-4'>
+        <DarkModeSwitch/>
       <Link href={'/'} className='text-2xl font-bold flex items-center py-1 px-2 rounded-lg bg-amber-400'>
         <span className=' text-black'>IN</span><span className='text-blue-950 hidden sm:inline'>seam</span>
       </Link>
+      </div>
     </div>
   )
 }
