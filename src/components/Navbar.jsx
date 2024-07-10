4023fc0b302938921551051d7fca3039272742a4
Avdhoot Kasture <45avdhootkasture8@gmail.com>
2024-07-10 10:56:41 +0530

    FETCH DATA FROM TMDB API

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import NavbarItem from './NavbarItem'
 export default function Navbar() {
   return (
     <div className='flex text-slate-900 font-extrabold dark:bg-amber-400 bg-amber-200 p-3 lg:text-lg justify-center gap-14'>
-      <NavbarItem title='Trending' param='fetchTrensing' />
+      <NavbarItem title='Trending' param='fetchTrending' />
       <NavbarItem title='Top Rated' param='fetchTopRated'/>
     </div>
   )
