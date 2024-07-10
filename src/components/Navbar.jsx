974a7ee4290309ffa66e6cec32efa01a94646f15
Avdhoot Kasture <45avdhootkasture8@gmail.com>
2024-07-10 12:42:08 +0530

    added card components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import NavbarItem from './NavbarItem'
 
 export default function Navbar() {
   return (
-    <div className='flex text-slate-900 font-extrabold dark:bg-amber-400 bg-amber-200 p-3 lg:text-lg justify-center gap-14'>
+    <div className='flex text-slate-900 font-extrabold dark:bg-slate-400 bg-amber-400 p-3 lg:text-lg justify-center gap-14'>
       <NavbarItem title='Trending' param='fetchTrending' />
       <NavbarItem title='Top Rated' param='fetchTopRated'/>
     </div>
